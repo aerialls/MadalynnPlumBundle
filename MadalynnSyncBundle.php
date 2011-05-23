63b9949bac74036765ac7bfbe5541cb7f5c0562e
MewT <mewt.fr@gmail.com>
2011-05-23 10:11:45 +0200

    initial commit

---
+++ MadalynnSyncBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Madalynn package.
+ *
+ * (c) 2010-2011 Julien Brochet <mewt.fr@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Madalynn\SyncBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MadalynnSyncBundle extends Bundle
+{
+}
