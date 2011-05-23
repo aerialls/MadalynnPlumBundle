97c49e036579fdc12c2ddc0d1c169befe9561920
MewT <mewt.fr@gmail.com>
2011-05-23 12:24:26 +0200

    changed the bundle name

--- MadalynnDeployBundle.php
+++ MadalynnDeployBundle.php
@@ -9,10 +9,10 @@
  * file that was distributed with this source code.
  */
 
-namespace Madalynn\SyncBundle;
+namespace Madalynn\DeployBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class MadalynnSyncBundle extends Bundle
+class MadalynnDeployBundle extends Bundle
 {
 }
