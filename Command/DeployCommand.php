128147feb7f0618b6c3d96e07038c924e43b1e9d
Julien Brochet <mewt.fr@gmail.com>
2011-06-14 15:45:10 +0200

    tweaked license

--- Command/DeployCommand.php
+++ Command/DeployCommand.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Madalynn package.
+ *
+ * (c) 2010-2011 Julien Brochet <mewt@madalynn.eu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Madalynn\DeployBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\Command;
